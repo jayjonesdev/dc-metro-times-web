6d11b7387f0e8cf22614861b26846b7087e385c6
Jerrell Jones <jayjonesdevelopment@gmail.com>
2022-06-05 18:39:08 -0400

    Add pulsing element to filled circle

--- src/components/FilledCircle/FilledCircle.component.tsx
+++ src/components/FilledCircle/FilledCircle.component.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Line, LineColor } from '../../types/rail.types';
-import './filledcircle.styles.css';
+import './filledCircle.styles.css';
 
 const FilledCircle: React.FC<{ line: Line; className?: string }> = ({
   line,
   className,
 }) => (
-  <div
-    className={`filled-circle ${LineColor[line]} ${className}`}
+  <span
     aria-label={`${line} line`}
     data-testid='line-filled-circle'
-  />
+    className={`flex h-3 w-3 ${className}`}
+  >
+    <span className={`pulse ${LineColor[line]}`}></span>
+    <span className={`filled-circle ${LineColor[line]}`}></span>
+  </span>
 );
 
 export default FilledCircle;
